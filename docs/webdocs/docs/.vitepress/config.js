47381ab71e284bc3db5eb8f840b8ecabf4cdf9f0
mkzi_nya <wcnmngsb999@gamil.com>
2026-03-12 08:52:03 +0800

    meow

--- docs/webdocs/docs/.vitepress/config.js
+++ docs/webdocs/docs/.vitepress/config.js
@@ -15,11 +15,25 @@ export default defineConfig({
         collapsed: false,
         items: [
           {
-            text: 'Story',
+            text: '语文',
             collapsible: true,
             collapsed: false,
             items: [
             { text: '蜀道の難', link: 'yuwf/udnj_jp' }
+            ]
+          },
+          {
+            text: '政治',
+            collapsible: true,
+            collapsed: false,
+            items: [
+            { text: '必修2', link: 'http://mkzi-nya.github.io/docs/vgvi/2.html' },
+            { text: '必修3', link: 'http://mkzi-nya.github.io/docs/vgvi/3.html' },
+            { text: '必修4', link: 'http://mkzi-nya.github.io/docs/vgvi/4.html' },
+            { text: '选必1', link: 'http://mkzi-nya.github.io/docs/vgvi/xb1.html' },
+            { text: '选必2', link: 'http://mkzi-nya.github.io/docs/vgvi/xb2.html' },
+            { text: '选必3', link: 'http://mkzi-nya.github.io/docs/vgvi/xb3.html' },
+            { text: '必修4_vite', link: 'vgvi/4_vite' }
             ]
           }
         ]
